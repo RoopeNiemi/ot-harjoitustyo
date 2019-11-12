3e9cf3111445f4bbc91ef9b7a428c62762ac4b12
elli kiiski <kiiski.elli@gmail.com>
2019-11-12 21:23:44 +0200

    toka tehtävä lisätty muistaakseni

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,44 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortinSaldoKasvaaOikein() {
+        kortti.lataaRahaa(200);
+        assertEquals("saldo: 12.0", kortti.toString());
+    }
+    
+    @Test
+    public void ostaminenVahentaaSaldoaOikein() {
+        kortti.otaRahaa(200);
+        assertEquals("saldo: 8.0", kortti.toString());
+    }
+    
+    @Test
+    public void ostaminenEiOnnistuJosEiOoTarpeeksFyrkkaa() {
+        kortti.otaRahaa(1200);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void liikaaOstaminenPalauttaaFalsen() {
+        assertTrue(!(kortti.otaRahaa(1200)));
+    }
+    
+    @Test
+    public void onnistunutOstaminenPalauttaaTruen() {
+        assertTrue(kortti.otaRahaa(200));
     }
 }
