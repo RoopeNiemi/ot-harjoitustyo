7480974b58802caca00cafd41aca595fbee42a80
elli kiiski <kiiski.elli@gmail.com>
2019-12-03 19:45:03 +0200

    uusi testi

--- projekti/ellin-sitsiprojekti/src/test/java/IlmojenHallintaTest.java
+++ projekti/ellin-sitsiprojekti/src/test/java/IlmojenHallintaTest.java
@@ -23,16 +23,21 @@ public class IlmojenHallintaTest {
         sitsit = new Sitsit("testisitsit");
         hallinta = new IlmojenHallinta();
         hallinta.setSitsit(sitsit);
-        taulukko = "#\tNimi\tSähköposti\tViini\tViina\tErityisruokavalio"
-                + "\n1.\tArhippa\ta.p@gmail.com\tPunkku\tKossu\tEn syö paprikaa"
-                + "\n2.\tAito-Iisakki Jimmynpoika\taito@jumalanterve.fi\tValkkari\tHoliton\tVegaani";
-        otsikot = new String[] {"#", "Nimi", "Sähköposti", "Viini", "Viina", "Erityisruokavalio"};
+        taulukko = "#\tNimi\tSähköposti\tViini\tViina\tErityisruokavalio\tEnsimmäinen fuksivuosi"
+                + "\n1.\tArhippa\ta.p@gmail.com\tPunkku\tKossu\tEn syö paprikaa\t2010"
+                + "\n2.\tAito-Iisakki Jimmynpoika\taito@jumalanterve.fi\tValkkari\tHoliton\tVegaani\t1998";
+        otsikot = new String[] {"#", "Nimi", "Sähköposti", "Viini", "Viina", "Erityisruokavalio", "Ensimmäinen fuksivuosi"};
     }
     
     @Test
     public void taulukonKasittelyToimii() {
         String[] kasiteltyTaulukko = hallinta.kasitteleTaulukko(taulukko);
         assertEquals(otsikot[0], kasiteltyTaulukko[0]);
+    }
+    
+    public void ilmojenMaaraOnOikein() {
+        hallinta.kasitteleTaulukko(taulukko);
+        assertEquals(2, hallinta.ilmojenMaara());
     }
     
     
